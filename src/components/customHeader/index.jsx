869b24303602d029c9e29a1517810a47a1f4d3a2
lokeshkumar12471 <105625414+lokeshkumar12471@users.noreply.github.com>
2023-04-19 01:31:52 +0530

    Two Components Created (Header, Chat)

---
+++ src/components/customHeader/index.jsx
@@ -0,0 +1,20 @@
+import React from "react"
+import { ChatBubbleLeftRightIcon, PhoneIcon } from "@heroicons/react/24/solid";
+const CustomHeader = ({ chat }) => {
+    console.log("chat.description",chat.description);
+    return (
+        <div className="chat-header">
+            <div className="flexbetween">
+                <ChatBubbleLeftRightIcon className="icon-chart" />
+                <h3 className="header-text">{chat.title}</h3>
+            </div>
+            <div className="flexbetween">
+                <PhoneIcon className="icon-phone" />
+                {chat.description !== "⬅️⬅️⬅️"   ?   (<p className="header-text">{chat.description}</p>) : (<p className="header-text">No Chat Selected</p>)}
+              
+               
+            </div>      
+        </div>
+    )
+}
+export default CustomHeader;
